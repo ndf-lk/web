5dc801fd23e097c56ed193fe05c4b3572c786d33
z9fr <z9fr@protonmail.com>
2022-08-21 11:33:43 +0530

    fixed some bugs with the join us form and hide form when submitted

--- src/pages/join.jsx
+++ src/pages/join.jsx
@@ -1,7 +1,6 @@
 import {
   SimpleGrid,
   Button,
-  Grid,
   Paper,
   Select,
   Container,
@@ -12,22 +11,29 @@ import {
   TextInput,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { LanguageContext } from "../context/userLangctx";
 import { useContext } from "react";
-import { getData } from "../data/getData";
 import { showNotification } from "@mantine/notifications";
 import { useMutation } from "@tanstack/react-query";
-import { getFormData } from "../data/formData";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Appconfig } from "../config";
 import axios from "axios";
+import { useLocalStorage } from "@mantine/hooks";
+
+// other imports
+import { LanguageContext } from "../context/userLangctx";
+import { getFormData } from "../data/formData";
+import { Appconfig } from "../config";
+import { getData } from "../data/getData";
 
 export const Join = () => {
   const BREAKPOINT = "@media (maxWidth: 800px)";
   const { language } = useContext(LanguageContext);
   const data = getData(language);
   const formdata = getFormData(language);
+  const [memberFormSubmitted, setMemberFormSubmission] = useLocalStorage({
+    key: "member-form-submission",
+    defaultValue: false,
+  });
 
   const [honorificSelection, setHonorificSelection] = useState(
     formdata.honorific_values[1].label
@@ -72,14 +78,21 @@ export const Join = () => {
           "Your membership request has been sent successfully, you will get a email after you get acceptd",
         color: "teal",
       });
+
+      setMemberFormSubmission(true);
     } catch (err) {
       showNotification({
-        title: "Error!",
-        message: err?.message,
+        title: err.response.data?.Message,
+        message: err?.response.data?.Error,
         color: "red",
       });
-      console.log(err);
+      console.log(JSON.stringify(err.response.data));
     }
+
+    // remove the added honorific
+    let namearr = values.name.split(" ");
+    namearr.shift();
+    values.name = namearr.join(" ");
   };
 
   return (
@@ -118,96 +131,158 @@ export const Join = () => {
         >
           {data.forthe}
         </Text>
-        <Container size={"sm"}>
-          <Paper shadow="xs" p="md" mt={30}>
-            <form onSubmit={form.onSubmit((values) => submitForm(values))}>
-              <Grid grow>
-                <Grid.Col span={1}>
-                  <Select
-                    value={honorificSelection}
-                    onChange={setHonorificSelection}
-                    label={formdata?.honorific}
-                    placeholder={formdata.honorific_values[1].label}
-                    data={formdata.honorific_values}
-                  />
-                </Grid.Col>
-                <Grid.Col span={7}>
+        <Container size={"md"} mb={100}>
+          <Paper shadow="xs" p="xl" mt={30} withBorder>
+            {memberFormSubmitted ? (
+              <>
+                <Title weight={900} mb={10} order={2}>
+                  {data?.memberformsubmission}
+                </Title>
+                <Text mb={20}>{data?.memberformdesc}</Text>
+
+                <Button
+                  type="submit"
+                  size="sm"
+                  variant="default"
+                  mb={10}
+                  onClick={() => setMemberFormSubmission(false)}
+                >
+                  {data?.submitanotherresponse}
+                </Button>
+              </>
+            ) : (
+              <>
+                <form onSubmit={form.onSubmit((values) => submitForm(values))}>
+                  <SimpleGrid
+                    cols={2}
+                    breakpoints={[
+                      { maxWidth: 980, cols: 2, spacing: "md" },
+                      { maxWidth: 755, cols: 1, spacing: "sm" },
+                      { maxWidth: 600, cols: 1, spacing: "sm" },
+                    ]}
+                  >
+                    <Select
+                      value={honorificSelection}
+                      onChange={setHonorificSelection}
+                      label={formdata?.honorific}
+                      placeholder={formdata.honorific_values[1].label}
+                      data={formdata.honorific_values}
+                      size="md"
+                    />
+
+                    <TextInput
+                      size="md"
+                      required
+                      label={formdata.name}
+                      placeholder="John Doe"
+                      {...form.getInputProps("name")}
+                    />
+                  </SimpleGrid>
+
                   <TextInput
+                    mt={20}
+                    size="md"
                     required
-                    label={formdata.name}
-                    placeholder="John Doe"
-                    {...form.getInputProps("name")}
+                    label={formdata.nic}
+                    placeholder={"790029871V"}
+                    {...form.getInputProps("nic")}
                   />
-                </Grid.Col>
-              </Grid>
 
-              <TextInput
-                mt={10}
-                required
-                label={formdata.nic}
-                placeholder={"790029871V"}
-                {...form.getInputProps("nic")}
-              />
+                  <SimpleGrid
+                    cols={2}
+                    breakpoints={[
+                      { maxWidth: 980, cols: 2, spacing: "md" },
+                      { maxWidth: 755, cols: 1, spacing: "sm" },
+                      { maxWidth: 600, cols: 1, spacing: "sm" },
+                    ]}
+                  >
+                    <TextInput
+                      required
+                      mt={20}
+                      size="md"
+                      label={formdata.phone}
+                      placeholder="+94751231234"
+                      {...form.getInputProps("phone")}
+                    />
 
-              <Group grow mt={10}>
-                <TextInput
-                  required
-                  label={formdata.phone}
-                  placeholder="+94751231234"
-                  {...form.getInputProps("phone")}
-                />
+                    <TextInput
+                      required
+                      mt={20}
+                      size="md"
+                      label={formdata.email}
+                      placeholder="your@email.com"
+                      {...form.getInputProps("email")}
+                    />
+                  </SimpleGrid>
 
-                <TextInput
-                  required
-                  label={formdata.email}
-                  placeholder="your@email.com"
-                  {...form.getInputProps("email")}
-                />
-              </Group>
+                  <TextInput
+                    mt={20}
+                    required
+                    label={formdata.address}
+                    size="md"
+                    placeholder={
+                      "No. 19/1A, Kolamunna Medapatha Road, Piliyandala"
+                    }
+                    {...form.getInputProps("address")}
+                  />
 
-              <TextInput
-                mt={10}
-                required
-                label={formdata.address}
-                placeholder={"No. 19/1A, Kolamunna Medapatha Road, Piliyandala"}
-                {...form.getInputProps("address")}
-              />
+                  <SimpleGrid
+                    cols={3}
+                    breakpoints={[
+                      { maxWidth: 980, cols: 3, spacing: "md" },
+                      { maxWidth: 755, cols: 1, spacing: "sm" },
+                      { maxWidth: 600, cols: 1, spacing: "sm" },
+                    ]}
+                  >
+                    <TextInput
+                      mt={20}
+                      required
+                      label={formdata.district}
+                      placeholder={"Colombo"}
+                      size="md"
+                      {...form.getInputProps("district")}
+                    />
 
-              <SimpleGrid
-                cols={3}
-                mt={10}
-                breakpoints={[
-                  { maxWidth: 980, cols: 3, spacing: "md" },
-                  { maxWidth: 755, cols: 1, spacing: "sm" },
-                  { maxWidth: 600, cols: 1, spacing: "sm" },
-                ]}
-              >
-                <TextInput
-                  required
-                  label={formdata.district}
-                  placeholder={"Colombo"}
-                  {...form.getInputProps("district")}
-                />
+                    <TextInput
+                      mt={20}
+                      required
+                      label={formdata.divisionalsecretariat}
+                      size="md"
+                      placeholder={"Piliyandala"}
+                      {...form.getInputProps("DivisionalSecretariat")}
+                    />
 
-                <TextInput
-                  required
-                  label={formdata.divisionalsecretariat}
-                  placeholder={"Piliyandala"}
-                  {...form.getInputProps("DivisionalSecretariat")}
-                />
+                    <TextInput
+                      mt={20}
+                      required
+                      label={formdata.villageofficerdomain}
+                      size="md"
+                      placeholder={"Western"}
+                      {...form.getInputProps("VillageOfficerDomain")}
+                    />
+                  </SimpleGrid>
 
-                <TextInput
-                  required
-                  label={formdata.villageofficerdomain}
-                  placeholder={"Western"}
-                  {...form.getInputProps("VillageOfficerDomain")}
-                />
-              </SimpleGrid>
-
-              <Group position="right" mt="md">
-                <Button type="submit">Submit</Button>
-              </Group>
-            </form>
+                  <Group position="right" mt={30}>
+                    <Button
+                      type="submit"
+                      size="md"
+                      fullWidth
+                      variant="gradient"
+                      color="dark"
+                      gradient={{
+                        from: Appconfig.lightcolor,
+                        to: "yellow",
+                      }}
+                      style={{
+                        color: "black",
+                      }}
+                    >
+                      Submit
+                    </Button>
+                  </Group>
+                </form>
+              </>
+            )}
           </Paper>
         </Container>
       </Container>
